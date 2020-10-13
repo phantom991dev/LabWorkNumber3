5534bf0a3699fc50fc381854a66603e50b4ac2ca
phantom991dev <72074402+phantom991dev@users.noreply.github.com>
2020-10-13 16:00:04 +0600

    code

--- LabWorkNumber5/LabWorkNumber5/Program.cs
+++ LabWorkNumber5/LabWorkNumber5/Program.cs
@@ -6,7 +6,28 @@ namespace LabWorkNumber5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool Stop = true;
+            int chislo;
+
+            while (Stop)
+            {
+                Console.WriteLine("Введите любое число из этого примера  z = 1 - x / 2 + y ^ 2 / 6 - x ^ 3 / 24 + y ^ 4 / 120 > ");
+                chislo = Convert.ToInt32(Console.ReadLine());
+
+                if (chislo == 0)
+                    break;
+                if (chislo == 5)
+                    break;
+                if (chislo == 7)
+                    break;
+                if (chislo == 8)
+                    break;
+                if (chislo == 9)
+                    break;
+                if (chislo == 10)
+                    break;
+
+            }
         }
     }
 }
